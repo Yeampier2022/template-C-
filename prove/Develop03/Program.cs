a6492fc42a4bbc189a3784f80b3932a7da0f74e7
Yeampier2022 <yeampierjesusa@gmail.com>
2023-05-27 08:24:35 -0400

    move folder at develop3

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,27 @@
 using System;
 
-class Programsf
+class Programw4
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Scripture scripture = new Scripture();
+        Reference reference = new Reference();
+        Words words = new Words();
+
+
+        Console.WriteLine("Welcome to Scripture Study");
+        Console.WriteLine();
+        Console.WriteLine("Please select a scripture favorite (1-5): ");
+
+        reference.DisplayAllReferences();
+        string choiceScripture = Console.ReadLine();
+        int i = Int32.Parse(choiceScripture);
+
+        Console.Clear();
+        Console.WriteLine(reference.DisplaySelectedReference(i));
+        words.HideWords(scripture.ScriptureArray(i), i);
+
+        Console.WriteLine();
+        Console.WriteLine("Thanks for Studying! Goodbye!");
     }
 }
