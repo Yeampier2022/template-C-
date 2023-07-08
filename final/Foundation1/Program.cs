990a358c3cba1e5fe7645cf921788abb36097b0b
Yeampier2022 <yeampierjesusa@gmail.com>
2023-07-07 21:19:05 -0400

    foundation 1 2

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -6,22 +6,22 @@ class Program
     {
         List<Video> _videos = new List<Video>{};
 
-        Video cats = new Video("cats", "charles dickens", 30);
-        cats.AddComment("susan", "why does his face look like that?");
-        cats.AddComment("david", "I miss my cat, she ran away");
-        cats.AddComment("Steve", "Click this link for free bitcoin bit.ly/56987");
+        Video cats = new Video("Dog", "charles dickens", 30);
+        cats.AddComment("Yeampier", "why does his face look like that?");
+        cats.AddComment("Marcelo", "I miss my dog, she ran away");
+        cats.AddComment("Gustavo", "Click this link for free bitcoin bit.ly/56987");
         _videos.Add(cats);
 
         Video puppies = new Video("puppies", "leroy jenkins", 60);
-        puppies.AddComment("karen", "Stop feeding him dog food, he deserves steaks");
-        puppies.AddComment("cesar", "What a cute doggy");
-        puppies.AddComment("Elon", "Your dog will be the first in space. Check your dms");
+        puppies.AddComment("Paola", "Stop feeding him dog food, he deserves steaks");
+        puppies.AddComment("Maria", "What a cute doggy");
+        puppies.AddComment("Gerardo", "Your dog will be the first in space. Check your dms");
         _videos.Add(puppies);
 
         Video conspiracy = new Video("Birds are government spies", "anonymous man", 100000);
-        conspiracy.AddComment("timmy", "very insightful, I will be hiding from birds now");
-        conspiracy.AddComment("eggman", "I am an egg");
-        conspiracy.AddComment("fbi", "We are watching your house");
+        conspiracy.AddComment("Leonardo", "very insightful, I will be hiding from birds now");
+        conspiracy.AddComment("Alejandro", "I am an egg");
+        conspiracy.AddComment("Fabricio", "We are watching your house");
         _videos.Add(conspiracy);
 
         foreach (Video v in _videos)
