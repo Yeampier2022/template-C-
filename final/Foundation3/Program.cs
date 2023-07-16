be6e18e0f87f6da22db7f47799f1f99f49e6fe25
Yeampier2022 <yeampierjesusa@gmail.com>
2023-07-15 23:00:40 -0400

    project done

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        List<Event> events = new List<Event>{};
+        Lecture lecture = new Lecture("Casey speaks", "Casey gives a lecture", "3/3/33", "3:33", "10505 ocean blvd", "casey", 25);
+        events.Add(lecture);
+
+
+        Outdoor outdoor = new Outdoor("Math social", "The math department meets to eat", "2/2/22", "10:00", "303 ricks garden", "Snowy");
+        events.Add(outdoor);
+
+
+        Reception reception = new Reception("jake's graduation", "we celebrate jake graduating", "5/4/2023", "9:00", "200 w 3rd south", "jake@email.com");
+        events.Add(reception);
+
+        foreach (Event e in events)
+        {
+            Console.WriteLine(e.FullDetails());
+            Console.WriteLine();
+            Console.WriteLine(e.StandardDetails());
+            Console.WriteLine();
+            Console.WriteLine(e.ShortDescription());
+            Console.WriteLine();
+        }
+    
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Exercise> exercises = new List<Exercise>{};
+
+        Running run = new Running("03 Nov 2022", 10.6, 1);
+        exercises.Add(run);
+
+        Swimming swim = new Swimming("04 Nov 2022", 30.6, 30);
+        exercises.Add(swim);
+
+        Cycling cycle = new Cycling("05 Nov 2022", 30.8, 10);
+        exercises.Add(cycle);
+
+        foreach (Exercise e in exercises)
+        {
+            Console.WriteLine();
+            Console.WriteLine(e.Summary());
+        }
+
     }
 }
